5b9298b95eeb254e4e608a819803a658d454237a
Somikyy <kirillshevchenko325@mail.ru>
2024-08-07 12:00:55 +0200

    commit
    few fixes

--- src/main/java/com/laybalt/Main.java
+++ src/main/java/com/laybalt/Main.java
@@ -22,7 +22,7 @@ import org.lwjgl.input.Keyboard;
 public class Main
 {
     public static final String MODID = "laybalt";
-    public static final String VERSION = "1.2-SNAPSHOT-1";
+    public static final String VERSION = "1.2-SNAPSHOT-2";
 
     private static final int keyBinding = Keyboard.KEY_RSHIFT;
 
